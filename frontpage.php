e580b2bbe254fb1b5c51a0a731731b54d4a74974
James Deromedi <jd1504a@student.american.edu>
2018-11-27 18:10:30 -0500

    The first framework for the website

---
+++ frontpage.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+	<title>CS Capstone</title>
+	<link rel="stylesheet" type="text/css" href="dabl_interface_style_sheet.css">
+
+</head>
+
+<body>
+
+<iframe width="100%" height="520" frameborder="0" src="https://jmderomedi.carto.com/builder/2a4e2686-6572-4229-a5a7-f7367b5cab1d/embed" allowfullscreen webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen></iframe>
+
+<div class = "wrapper">
+
+	<h1><p class = "header">This is the start of the capstone project </p></h1>
+
+</div>
+	
+	
+</body>
+</html>
